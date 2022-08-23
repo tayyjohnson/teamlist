c8bf5a3c39cbde0f77dd5e62d091cb9a76e3c49f
tayyjohnson <taydriver1@gmail.com>
2022-08-22 20:31:08 -0400

    code working wonderfully & video demo done

---
+++ index.js
@@ -0,0 +1,242 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const generateSite = require('./src/generate-site.js');
+const teamMembers = [];
+
+//list that options that the creators has for their team
+const siteOptions = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'Please select the team member you would like to add:',
+            choices: ['add a manager', 'add an engineer', 'add an intern', 'finish building my team']
+        }
+    ])
+    .then(userChoice => {
+        switch (userChoice.menu) {
+            //after a case is chosen, the user will be directed to the associated function
+            case 'add a manager':
+                managerInfo();
+                break;
+            case 'add an engineer':
+                engineerInfo();
+                break;
+            case 'add an intern':
+                internInfo();
+                 break;
+            default:
+                //if the user is done entering information then the page will run using the create page function at the bottom of this code
+                createPage();
+        };
+    });
+};
+
+//function to collect info for a manager
+const managerInfo = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name : 'name',
+            message: "What is the manager's name?",
+            validate : name => {
+                if (name) {
+                    return true;
+                }else {
+                    console.log('Please enter manager name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name : 'employeeId',
+            message: "What is the manager's employee ID?",
+            validate : employeeId => {
+                if (employeeId) {
+                    return true;
+                }else {
+                    console.log('Please enter manager ID #!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name : 'email',
+            message: "What is the manager's email address?",
+            validate : email => {
+                if (email) {
+                    return true;
+                }else {
+                    console.log('Please enter manager email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name : 'officeNumber',
+            message: "What is the manager's office number?",
+            validate : officeNumber => {
+                if (officeNumber) {
+                    return true;
+                }else {
+                    console.log('Please enter manager office #!');
+                    return false;
+                }
+            }
+        },
+    ])
+    .then(input => {
+        console.log(input);
+        const manager = new Manager(input.name, input.employeeId, input.email, input.officeNumber);
+        teamMembers.push(manager);
+        //send the user back tot he main menu
+        siteOptions();
+    });
+};
+
+//function to collect info for an engineer
+const engineerInfo = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name : 'name',
+            message: "What is the engineer's name?",
+            validate : name => {
+                if (name) {
+                    return true;
+                }else {
+                    console.log('Please enter engineer name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name : 'employeeId',
+            message: "What is the engineer's employee ID?",
+            validate : employeeId => {
+                if (employeeId) {
+                    return true;
+                }else {
+                    console.log('Please enter engineer ID #!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name : 'email',
+            message: "What is the engineer's email address?",
+            validate : email => {
+                if (email) {
+                    return true;
+                }else {
+                    console.log('Please enter engineer email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name : 'gitHub',
+            message: "What is the engineer's GitHub username?",
+            validate : gitHub => {
+                if (gitHub) {
+                    return true;
+                }else {
+                    console.log("Please enter engineer's GitHub username!");
+                    return false;
+                }
+            }
+        },
+    ])
+    .then(input => {
+        console.log(input);
+        const engineer = new Engineer(input.name, input.employeeId, input.email, input.gitHub);
+        teamMembers.push(engineer);
+        //returns user to the main menu
+        siteOptions();
+    });
+};
+
+//function to collect info for an intern
+const internInfo = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name : 'name',
+            message: "What is the intern's name?",
+            validate : name => {
+                if (name) {
+                    return true;
+                }else {
+                    console.log('Please enter intern name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name : 'employeeId',
+            message: "What is the intern's employee ID?",
+            validate : employeeId => {
+                if (employeeId) {
+                    return true;
+                }else {
+                    console.log('Please enter intern ID #!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name : 'email',
+            message: "What is the intern's email address?",
+            validate : email => {
+                if (email) {
+                    return true;
+                }else {
+                    console.log('Please enter intern email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name : 'school',
+            message: "What is the intern's school name?",
+            validate : school => {
+                if (school) {
+                    return true;
+                }else {
+                    console.log("Please enter intern's school name!");
+                    return false;
+                }
+            }
+        },
+    ])
+    .then(input => {
+        console.log(input);
+        const intern = new Intern(input.name, input.employeeId, input.email, input.school);
+        teamMembers.push(intern);
+        //returns user to the main menu
+        siteOptions();
+    });
+
+};
+    //use above info to create the finished page
+    const createPage = () => {
+        fs.writeFileSync('./dist/index.html', generateSite(teamMembers)
+
+        ); 
+    };
+
+
+siteOptions();
--- src/generate-site.js
+++ src/generate-site.js
@@ -1,10 +1,70 @@
-const Employee = require('../lib/Employee');
-const Manager = require('../lib/Manager');
-const Intern = require('../lib/Intern');
-const Engineer = require('../lib/Engineer');
+const fs = require('fs');
+const path = require('path');
+const html = []
 
-//function to generate team html
-function generateSite (manager, engineer, intern) {
+
+const generateTeam = (employeeStuff) => {
+    const generateManager = manager => {
+        return `
+        <div class="card">
+            <div class="card-header"> 
+            ${manager.getName()} <br/>
+                <div class="workerTitle">Manager</div>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"> ID: ${manager.getId()}</li>
+                <li class="list-group-item"> Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item"> Office Number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>`; 
+         
+    }
+
+    const generateEngineer = engineer => {
+        return `
+        <div class="card">
+            <div class="card-header"> 
+            ${engineer.getName()} <br/>
+                <div class="workerTitle">Engineer</div>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"> ID: ${engineer.getId()}</li>
+                <li class="list-group-item"> Email: <span id='email'><a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></span></li>
+                <li class="list-group-item">Github Username: <a target="_blank" href="https://github.com/${engineer.getGitHub()}">${engineer.getGitHub()}</a></li>
+            </ul>
+        </div>`;
+             
+    };
+
+    const generateIntern = intern => {
+        return `
+        <div class="card">
+            <div class="card-header"> 
+            ${intern.getName()} <br/>
+                <div class="workerTitle">Intern</div>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"> ID: ${intern.getId()}</li>
+                <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+                <li class="list-group-item"> School: ${intern.school}</li>
+            </ul>
+        </div>`;
+         
+    }
+    html.push(employeeStuff.filter(employee => employee.getRole() === 'Manager').map(manager => generateManager(manager)));
+
+    html.push(employeeStuff.filter(employee => employee.getRole() === 'Engineer').map(engineer => generateEngineer(engineer)));
+
+    html.push(employeeStuff.filter(employee => employee.getRole() === 'Intern').map(intern => generateIntern(intern)));
+
+    //join HTML together
+    return html.join('');
+
+}
+
+
+
+module.exports = employeeStuff => {
     return `
     <!DOCTYPE html>
 <html lang="en">
@@ -13,61 +73,18 @@ function generateSite (manager, engineer, intern) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <title>Team Roster</title>
+    <link rel='stylesheet' href='../dist/style.css'/>
+    <title>Team Profile Generator</title>
 </head>
 <body>
-    <header class="header">
-        <h1>Hey Team!</h1>
+    <header>
+        <h1> My Team </h1>
     </header>
+    <main>
+    ${generateTeam(employeeStuff)}
+    </main>
 </body>
-</html>`
+<script src="../src/generate-site.js"></script>
+</html>
+    `
 }
-
-const generateManager = manager => {
-    let managerSection = `
-    <div class="card">
-        <div class="card-header"> 
-        ${manager.name} <br/>
-        Manager
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item"> ID: ${manager.id}</li>
-            <li class="list-group-item"> Email: <a href="mailto:${manager.email}>${manager.email}</a></li>
-            <li class="list-group-item"> Office Number: ${manager.officeNumber}</li>
-        </ul>
-    </div>`;    
-}
-
-const generateEngineer = engineer => {
-    let engineerSection = `
-    <div class="card">
-        <div class="card-header"> 
-        ${engineer.name} <br/>
-        Engineer
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item"> ID: ${engineer.id}</li>
-            <li class="list-group-item"> Email: <a href="mailto:${engineer.email}>${engineer.email}</a></li>
-            <li class="list-group-item"> GitHub Username: <a target="_blank" href="https://github.com/${engineer.gitHub}">${engineer.gitHub}</a></li>
-        </ul>
-    </div>`;    
-};
-
-const generateIntern = intern => {
-    let internSection = `
-    <div class="card">
-        <div class="card-header"> 
-        ${intern.name} <br/>
-        Intern
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item"> ID: ${intern.id}</li>
-            <li class="list-group-item"> Email: <a href="mailto:${intern.email}>${intern.email}</a></li>
-            <li class="list-group-item"> School: ${intern.school}</li>
-        </ul>
-    </div>`;    
-}
-
-
-module.export = generateSite;
